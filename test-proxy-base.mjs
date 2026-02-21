40cb822cfd9f8e0e4cc90732d7de334b97c6a3b1
kdaas-gis <kdaasgcp@gmail.com>
2026-02-21 15:08:39 +0530

    chore: ignore python and mjs files

--- test-proxy-base.mjs
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-
-async function run() {
-  const url = 'http://localhost:3004/landstack/api/proxy?url=http%3A%2F%2F117.252.86.213%3A8080%2Fgeoserver%2Fapplication%2Fwms&REQUEST=GetMap&SERVICE=WMS&VERSION=1.1.1&FORMAT=image%2Fpng&STYLES=&TRANSPARENT=true&LAYERS=application%3Adistrict_boundary&TILED=true&WIDTH=256&HEIGHT=256&SRS=EPSG%3A4326&BBOX=75.9375%2C14.0625%2C78.75%2C16.875';
-  const res = await fetch(url);
-  console.log(res.status, res.headers.get('content-type'));
-  console.log('x-proxy-auth:', res.headers.get('x-proxy-auth'));
-}
-run();
